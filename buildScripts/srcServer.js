94df30dcd9eabc8cf0678066d7e82424e39c6363
Shubham Naithani <-adm-nasdbv-ams@Bp1azutt.ad.bpazure.com>
2018-08-06 13:02:22 +0000

    http request first half

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,8 +16,18 @@ app.use(require('webpack-dev-middleware')(compiler,{
   noInfo: true,
   publicPath: config.output.publicPath
 }));
+
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.get('/users', function(req,res){
+  //hard coding for simplicity. Pretend this hits a real data base
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"lee","email":"lee.tina@hotmail.com"}
+  ]);
 });
 
 app.listen(port, function(err){
