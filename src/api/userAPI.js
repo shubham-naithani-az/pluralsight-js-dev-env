37bc6b0e7326d7f1e523c0b07e3271e8d8e916d8
Shubham Naithani <shubham.naithani.az@hotmail.com>
2018-08-07 00:33:45 +0530

    final commit after http calls

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,13 +1,23 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers(){
     return get('users');
 }
-
-function get(url){
-    return fetch(url).then(onSuccess,onError);
+export function deleteUser(id){
+  return del(`users/${id}`);
 }
-
+function get(url){
+    return fetch(baseUrl + url).then(onSuccess,onError);
+}
+function del(url){
+  const request = new Request(baseUrl + url,{
+    method: 'Delete'
+  });
+  return fetch(request).then(onSuccess, onError);
+}
 function onSuccess(response){
     return response.json();
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {getUsers} from './api/userAPI';
+import {getUsers,deleteUser} from './api/userAPI';
 
 //populate table of users via API call.
 
 getUsers().then(result =>{
     let usersBody="";
-    
+
     result.forEach(user => {
     usersBody+=`<tr>
     <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
@@ -12,7 +12,19 @@ getUsers().then(result =>{
     <td>${user.firstName}</td>
     <td>${user.lastName}</td>
     <td>${user.email}</td>
-    </tr>`      
+    </tr>`
     });
     global.document.getElementById('users').innerHTML=usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link=>{
+      link.onclick = function(event) {
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.attributes["data-id"].value);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+      };
+    });
 });
